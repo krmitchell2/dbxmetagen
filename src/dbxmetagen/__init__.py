6a45e23fe956b399baf435881e7406697253f032
Kaylah Rose Mithell <kmitchell@hsh.lacounty.gov>
2026-02-24 12:54:25 -0800

    Remove init stuff modules

--- src/dbxmetagen/__init__.py
+++ src/dbxmetagen/__init__.py
@@ -1,17 +1,17 @@
-__version__ = "0.5.3"
+# __version__ = "0.5.3"
 
-from src.dbxmetagen.config import MetadataConfig
-from src.dbxmetagen.overrides import (
-    build_condition,
-    apply_overrides_with_loop,
-    apply_overrides_with_joins,
-    override_metadata_from_csv,
-)
+# from src.dbxmetagen.config import MetadataConfig
+# from src.dbxmetagen.overrides import (
+#     build_condition,
+#     apply_overrides_with_loop,
+#     apply_overrides_with_joins,
+#     override_metadata_from_csv,
+# )
 
-__all__ = [
-    "MetadataConfig",
-    "build_condition",
-    "apply_overrides_with_loop",
-    "apply_overrides_with_joins",
-    "override_metadata_from_csv",
-]
+# __all__ = [
+#     "MetadataConfig",
+#     "build_condition",
+#     "apply_overrides_with_loop",
+#     "apply_overrides_with_joins",
+#     "override_metadata_from_csv",
+# ]
